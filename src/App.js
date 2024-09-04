a1184b63769edc1bbb0def71765a04bc82277e6a
Ben Abbott <babbott85@gmail.com>
2024-09-04 10:18:05 -0600

    auth0 logout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LogTest from './components/logtest'
+import Logout from './components/logout'
 function App() {
   return (
     <div className="App">
       {/* <ModalL/> */}
       <LogTest/>
+      <Logout/>
     </div>
   );
 }
